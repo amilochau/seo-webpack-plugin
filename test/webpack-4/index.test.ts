aeee720fbae02626e032335fffbb53f5b8ddd400
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-12-20 18:51:47 +0100

    Mock dates

--- test/webpack-4/index.test.ts
+++ test/webpack-4/index.test.ts
@@ -1,6 +1,9 @@
 import webpack from "webpack";
 import generateCases from "../utils/cases";
 
+jest.useFakeTimers()
+jest.setSystemTime(new Date('2022-01-01'))
+
 describe("webpack 4", () => {
   generateCases(webpack);
 });
--- test/webpack-5/index.test.ts
+++ test/webpack-5/index.test.ts
@@ -1,6 +1,9 @@
 import webpack from "webpack";
 import generateCases from "../utils/cases";
 
+jest.useFakeTimers()
+jest.setSystemTime(new Date('2022-01-01'))
+
 describe("webpack 5", () => {
   generateCases(webpack);
 });
